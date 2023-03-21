7bfd992b92188e969f72fbd7c61c7996412c5403
moaaz97 <moaaz.briek97@gmail.com>
2023-03-21 23:03:01 +0200

    Change the dashboard background

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -11,3 +11,6 @@
 @section('contentHeaderActive')
     عرض
 @endsection
+@section('content')
+<div class="row" style="background-image: url({{asset('assets/admin/imgs/dash.jpg')}}); background-size:cover; background-repeat: no-repeat; min-height: 600px "></div>
+@endsection
