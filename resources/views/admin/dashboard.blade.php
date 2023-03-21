3c8bae714023d8d3018952c96483159fbcdfc416
moaaz97 <moaaz.briek97@gmail.com>
2023-03-21 22:33:54 +0200

    Create the dashboard view

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.admin');
+@section('title')
+    الرئيسية
+@endsection
+@section('contentHeader')
+    الرئيسية
+@endsection
+@section('contentHeaderLink')
+    <a href="{{route('admin.dashboard')}}">Link</a>
+@endsection
+@section('contentHeaderActive')
+    عرض
+@endsection
