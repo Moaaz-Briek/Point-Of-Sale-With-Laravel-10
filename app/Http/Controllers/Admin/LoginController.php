51736b47a912a658b0106ef4fe17e06d6bf9b2ca
moaaz97 <moaaz.briek97@gmail.com>
2023-03-28 00:17:48 +0200

    Logout method

--- app/Http/Controllers/Admin/LoginController.php
+++ app/Http/Controllers/Admin/LoginController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
-use Illuminate\Http\RedirectResponse as RedirectResponseAlias;
 
 class LoginController extends Controller
 {
@@ -13,11 +12,17 @@ class LoginController extends Controller
         return view('admin.auth.login');
     }
 
-    public function login(LoginRequest $request): RedirectResponseAlias
+    public function login(LoginRequest $request)
     {
         if (auth()->guard('admin')->attempt(['username' => $request->input('username'), 'password' => $request->input('password')])) {
             return redirect()->route('admin.dashboard');
         }
         return back();
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+        return redirect()->route('admin.home');
     }
 }
